c3ab3cfed50de8ced4b801bc971bf2d59e2075d4
s7minhas <s7.minhas@gmail.com>
2017-11-11 15:33:18 -0500

    data setup

--- R/data/1_buildNetData.R
+++ R/data/1_buildNetData.R
@@ -1,1 +1,15 @@
-if(Sys.info()['user'] %in% c('s7m', 'janus829')){ source('~/Research/intraConfNetDyn/R/setup.R') }
+if(Sys.info()['user'] %in% c('s7m', 'janus829')){
+	source('~/Research/intraConfNetDyn/R/setup.R') }
+
+#
+load(paste0(pathData, 'ged171.Rdata')) ; ged=ged171 ; rm(ged171)
+
+ged = ged[,c(
+	'id','year','type_of_vi','conflict_n',
+	'dyad_new_i','dyad_name',
+	'side_a_new','side_a',
+	'side_b_new','side_b',
+	'country',
+	'date_start','date_end',
+	'best', 'deaths_a', 'deaths_b', 'deaths_civ'
+	)]
--- R/setup.R
+++ R/setup.R
@@ -1,4 +1,36 @@
+########
 rm(list=ls())
-if(Sys.info()['user'] %in% c('s7m', 'janus829')){ pathDrop = '~/Dropbox/Research/intraConfNetDyn' ; pathData = paste0(pathDrop, 'data') }
+if(Sys.info()['user'] %in% c('s7m', 'janus829')){
+	pathGit = '~/Research/intraConfNetDyn/'
+	pathDrop = '~/Dropbox/Research/intraConfNetDyn/'
+	pathData = paste0(pathDrop, 'data/')
+}
+########
 
+########
+# misc
+char = function(x){ as.character(x) }
+num = function(x){ as.numeric(char(x)) }
+trim = function (x) { gsub("^\\s+|\\s+$", "", x) }
+########
 
+########
+# install/load libraries
+loadPkg=function(toLoad){
+	for(lib in toLoad){
+	  if(!(lib %in% installed.packages()[,1])){ 
+	    install.packages(lib, repos='http://cran.rstudio.com/') }
+	  suppressMessages( library(lib, character.only=TRUE) )
+	}
+}
+
+pkgs = c(
+	'dplyr', 'tidyr', 'magrittr',
+	'network', 'igraph', 
+	'ggplot2', 'RColorBrewer', 'latex2exp'
+	)
+loadPkg(pkgs)
+
+# Set a theme for gg
+theme_set(theme_bw())
+########
