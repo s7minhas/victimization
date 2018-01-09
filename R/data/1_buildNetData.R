4cb39415c5624c765442bc4aeb7cca86d9f92ed5
s7minhas <s7.minhas@gmail.com>
2018-01-09 14:20:37 -0500

    civ plots

--- R/data/1_buildNetData.R
+++ R/data/1_buildNetData.R
@@ -126,50 +126,36 @@ summStatsACLED_toFocus = data.frame(do.call('rbind', lapply(toFocus, function(c)
 		cntConf = nrow(slice)
 		cntActors = length(unique(c(slice$ACTOR1, slice$ACTOR2)))
 		cntDyads = length(unique(slice$dyad_name))
-		c(cntry=c, year=t, cntConf=cntConf, cntActors=cntActors, cntDyads=cntDyads)
+		sliceCiv = acledCiv[acledCiv$COUNTRY==c & acledCiv$YEAR==t,]
+		cntCivEvents = nrow(sliceCiv)
+		cntCivFatals = ifelse(nrow(sliceCiv)>0, sum(sliceCiv$FATALITIES), 0)
+		c(cntry=c, year=t, 
+			cntConf=cntConf, cntActors=cntActors, cntDyads=cntDyads, 
+			cntCivEvents=cntCivEvents, cntCivFatals=cntCivFatals)
 	}) )
 })), stringsAsFactors=FALSE)
 
-for(v in c('year','cntConf','cntActors','cntDyads')){ summStatsACLED_toFocus[,v]=num(summStatsACLED_toFocus[,v])}
+for(v in setdiff(names(summStatsACLED_toFocus), 'cntry')){ summStatsACLED_toFocus[,v]=num(summStatsACLED_toFocus[,v])}
 
-g=ggplot(summStatsACLED_toFocus, aes(x=year, y=cntConf, group=1)) +
-	geom_line() +
-	geom_point() +
-	ylab("Number of conflicts") + 
-	scale_x_continuous('', breaks=seq(2000,2016,2)) +
-	facet_wrap(~cntry, scales='free_y', ncol=3) +
-	theme_bw() +
-	theme(
-		axis.text.x=element_text(angle=45, hjust=1, size=6),
-		axis.ticks=element_blank(),
-		panel.border=element_blank()
-		)
-ggsave(g, file=paste0(pathDrop, 'cntConf.pdf'), width=9, height=5)
+plotSumm = function(data, yVar, yLab, fName){
+	data$ggY = data[,yVar]
+	g=ggplot(data, aes(x=year, y=ggY, group=1)) +
+		geom_line() +
+		geom_point() +
+		ylab(yLab) + 
+		scale_x_continuous('', breaks=seq(2000,2016,2)) +
+		facet_wrap(~cntry, scales='free_y', ncol=3) +
+		theme_bw() +
+		theme(
+			axis.text.x=element_text(angle=45, hjust=1, size=6),
+			axis.ticks=element_blank(),
+			panel.border=element_blank()
+			)
+	ggsave(g, file=paste0(pathDrop, fName), width=9, height=5)	
+}
 
-g=ggplot(summStatsACLED_toFocus, aes(x=year, y=cntActors, group=1)) +
-	geom_line() +
-	geom_point() +
-	ylab("Number of actors") + 
-	scale_x_continuous('', breaks=seq(2000,2016,2)) +
-	facet_wrap(~cntry, scales='free_y', ncol=3) +
-	theme_bw() +
-	theme(
-		axis.text.x=element_text(angle=45, hjust=1, size=6),
-		axis.ticks=element_blank(),
-		panel.border=element_blank()
-		)
-ggsave(g, file=paste0(pathDrop, 'cntActors.pdf'), width=9, height=5)
-
-g=ggplot(summStatsACLED_toFocus, aes(x=year, y=cntDyads, group=1)) +
-	geom_line() +
-	geom_point() +
-	ylab("Number of dyads") + 
-	scale_x_continuous('', breaks=seq(2000,2016,2)) +
-	facet_wrap(~cntry, scales='free_y', ncol=3) +
-	theme_bw() +
-	theme(
-		axis.text.x=element_text(angle=45, hjust=1, size=6),
-		axis.ticks=element_blank(),
-		panel.border=element_blank()
-		)
-ggsave(g, file=paste0(pathDrop, 'cntDyads.pdf'), width=9, height=5)
+plotSumm(summStatsACLED_toFocus, 'cntConf', 'Number of conflicts', 'cntConf.pdf')
+plotSumm(summStatsACLED_toFocus, 'cntActors', 'Number of actors', 'cntActors.pdf')
+plotSumm(summStatsACLED_toFocus, 'cntDyads', 'Number of dyads', 'cntDyads.pdf')
+plotSumm(summStatsACLED_toFocus, 'cntCivEvents', 'Number of Violent Civilian Events', 'cntCivEvents.pdf')
+plotSumm(summStatsACLED_toFocus, 'cntCivFatals', 'Number of Civilian Fatalities', 'cntCivFatals.pdf')
