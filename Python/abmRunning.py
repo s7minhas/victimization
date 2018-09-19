6392604bf72301f17d67da87f41ba185ed5b544d
mgallop <max.gallop@gmail.com>
2018-09-19 11:47:17 +0100

    more abm sims

--- Python/abmRunning.py
+++ Python/abmRunning.py
@@ -1,7 +1,7 @@
 from VicABM import *
 import csv
 
-with open('abmresults1.csv', 'w') as csvfile:
+with open('abmresultsFinal.csv', 'w') as csvfile:
   abmwriter = csv.writer(csvfile, delimiter=',')
   for i in range(10000):
     territory = np.random.poisson(20)
@@ -14,7 +14,7 @@ with open('abmresults1.csv', 'w') as csvfile:
     delta = np.random.uniform(low = .1, high = .5)
     battledeaths = np.random.poisson(1) + 1
     growthrate = .1
-    victimerror = np.random.uniform(low = 0, high = .15)
+    victimerror = np.random.uniform(low = 0, high = .5)
     turnlimit = np.random.poisson(10) + 1
     abm = Country("abm", territory, conn, actors, civ)
     abm.Game()
