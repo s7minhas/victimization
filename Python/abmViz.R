d027b3b1379d5e10462b08c2f556fe1924fef92e
s7minhas <s7.minhas@gmail.com>
2019-02-28 08:32:44 -0500

    function to check for terr change

--- Python/abmViz.R
+++ Python/abmViz.R
@@ -14,9 +14,36 @@ abmData = read.csv(paste0(abmPath, 'abmViz.csv'), header=FALSE)
 # V15 = adjacency matrix
 
 ###hmmmm, dont see any territory turnover
-# as.character(abmData[1,'V14'])
-# as.character(abmData[10,'V14'])
-# as.character(abmData[50,'V14'])
+abmData$V14 = char(abmData$V14)
+terrChange = rep(NA, nrow(abmData))
+for(i in 1:nrow(abmData)){
+	x = strsplit(abmData[i,'V14'], '[0:9]*: ')
+	x = x[[1]][-1]
+	x = lapply(x, function(z){gsub('\\], [0-9]*', '', z)})
+	x = lapply(x, function(z){gsub('}', '', z, fixed=TRUE)})
+	x = lapply(x, function(z){substring(z,2)})
+	x[[length(x)]] = substring(x[[length(x)]], 1, nchar(x[[length(x)]])-1)
+
+	x = lapply(x, function(z){
+		z = gsub(', ', ' ', z)
+		z = gsub('\\(|\\)','',z)
+
+		terrs = gsub('\\[|\\]','',trim(strsplit(z, '[A-Z]')[[1]][-1]))
+		actors = regmatches(z, gregexpr("[[:alpha:]]+", z))[[1]]
+		names(terrs) = actors
+		return(sort(terrs)) })
+	out = ifelse(length(unique(x))==1, 'no territory change', 'woohoo')
+	terrChange[i] = out }
+
+# abmData[1,'V14']
+# x
+# x[[1]]
+# x[[11]]
+
+
+as.character(abmData[1,'V14'])
+as.character(abmData[10,'V14'])
+as.character(abmData[50,'V14'])
 
 # clean stuff up
 abmData$V12 = char(abmData$V12)
