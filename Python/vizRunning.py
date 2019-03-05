8726dd0e58e1e3d5a0994bcb89fb837debe8d9c5
mgallop <max.gallop@gmail.com>
2019-03-05 12:47:23 +0000

    more simulation mods

--- Python/vizRunning.py
+++ Python/vizRunning.py
@@ -1,18 +1,18 @@
 from VicForViz import *
 import csv
 
-with open('terrBigRun.csv', 'w') as csvfile:
+with open('ActorVarRun.csv', 'w') as csvfile:
   abmwriter = csv.writer(csvfile, delimiter=',')
-  for i in range(3000):
-    actors = 10
+  for i in range(5000):
+    actors = np.random.poisson(10)
     territory = max(np.random.poisson(13), actors + 1)
     conn = np.random.uniform(low = .2, high = .75)
-    civ = np.random.poisson(75)
-    VicPenalty = np.random.uniform(low = .05, high = .3)
+    civ = 75
+    VicPenalty = np.random.uniform(low = .01, high = .5)
     CoerceMob = np.random.uniform(low = .1, high = .5)
     DisloyalPenalty = np.random.uniform(low = .25, high = .75)
     delta = np.random.uniform(low = .1, high = .3)
-    battledeaths = np.random.poisson(1) + 1
+    battledeaths = np.random.poisson(5) + 1
     growthrate = .1
     victimerror = np.random.uniform(low = 0, high = .5)
     turnlimit = np.random.poisson(15) + 1
