566abc01f789b6cff701f896a3c7d151626b7276
s7minhas <s7.minhas@gmail.com>
2019-03-21 10:48:40 -0400

    set up out of sample test

--- R/analysis/1_runModels.R
+++ R/analysis/1_runModels.R
@@ -24,20 +24,6 @@ data$nConf[is.na(data$nConf)] = 0
 ########################################################
 
 ########################################################
-# run models
-dv = 'civVicCount'
-vars = c(
-	'graph_dens', 'graph_recip', 'graph_trans', 
-	'nConf', 'nActors', 
-	'polity2'
-	# 'rebsStronger',
-	,'ethTens', 'anyPeaceKeeper'
-	# 'rebSupportGov', 'govSupportGov'	
-	)
-modData=data
-########################################################
-
-########################################################
 # naive just impute everything
 loadPkg('sbgcop')
 if(!file.exists(paste0(pathData, 'imputedData_acledUndirected.rda'))){
@@ -61,6 +47,71 @@ iData = lapply(500:1000, function(i){
 
 ########################################################
 # run base model
+modBase_noImp = glm.nb(
+	civVicCount ~  # dv
+		graph_dens + nConf + nActors + factor(cname) -1 
+	, data=data
+	)
+summBase_noImp = summary(modBase_noImp)$'coefficients'
+round(summBase_noImp[!grepl('factor',rownames(summBase_noImp)),], 3)
+
+
+# out of sample cross-val
+set.seed(6886)
+nFolds = nrow(data)
+# data$fold = sample(1:nFolds, nrow(data), replace=TRUE)
+data$fold = 1:nFolds
+
+rmseVec1 = NULL
+rmseVec2 = NULL
+vars = c('civVicCount','graph_dens','nConf','nActors','cname')
+for(f in 1:nFolds){
+
+	# split sample
+	train = data[data$fold!=f,]
+	test = data[data$fold==f,]
+
+	# mod1
+	mod1 = glm.nb(
+		civVicCount ~  # dv
+			graph_dens + nConf + nActors + factor(cname) -1,
+			data=train )
+
+	# mod2
+	mod2 = glm.nb(
+		civVicCount ~  # dv
+			nConf + nActors + factor(cname) -1,
+			data=train )
+
+	# pull out relev vars
+	yObs = test[,vars[1]]
+	testData1 = test[,vars[-1]]
+	testData2 = test[,vars[-c(1:2)]]
+
+	# pull out relev betas
+	beta1 = coef(mod1)[1:3]
+	beta1 = c(beta1, coef(mod1)[grepl(testData1$cname,names(coef(mod1)))])
+
+	beta2 = coef(mod2)[1:2]
+	beta2 = c(beta2, coef(mod2)[grepl(testData2$cname,names(coef(mod2)))])	
+
+	# make data a matrix
+	testData1[,'cname'] = 1
+	testData1 = data.matrix(testData1)
+
+	testData2[,'cname'] = 1
+	testData2 = data.matrix(testData2)	
+
+	# generate predictions
+	yhat1 = exp(beta1 %*% t(testData1))
+	rmseVec1 = c(rmseVec1, (yhat1 - yObs)^2)
+	yhat2 = exp(beta2 %*% t(testData2))
+	rmseVec2 = c(rmseVec2, (yhat2 - yObs)^2)
+}
+
+sqrt(mean(rmseVec1, na.rm=TRUE))
+sqrt(mean(rmseVec2, na.rm=TRUE))
+
 modsBase = lapply(iData, function(modData){
 	mod = glm.nb(
 		civVicCount ~  # dv
@@ -88,8 +139,9 @@ modsCntrls = lapply(iData, function(modData){
 
 # summarize
 summBase = rubinCoef(modsBase, TRUE)
-round(summBase[!grepl('factor',rownames(summBase)),], 3)
-
 summCntrls = rubinCoef(modsCntrls, TRUE)
+
+#
+round(summBase[!grepl('factor',rownames(summBase)),], 3)
 round(summCntrls[!grepl('factor',rownames(summCntrls)),], 3)
 ########################################################
