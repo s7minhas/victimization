46d3542fdc01611bc0e2c937f13f325c3591727a
mgallop <max.gallop@gmail.com>
2020-05-29 15:23:43 +0100

    allow gov ideology to be moderate or random

--- Python/abmRunning.py
+++ Python/abmRunning.py
@@ -3,24 +3,25 @@ import csv
 
 with open('abmNewSelectProb.csv', 'w') as csvfile:
   abmwriter = csv.writer(csvfile, delimiter=',')
-  for i in range(1000):
-    actors = 10
+  for i in range(3000):
+    actors = nr.random.poisson(10)
     territory = max(np.random.poisson(13), actors + 1)
     conn = np.random.uniform(low = .2, high = .75)
     civ = np.random.poisson(45)
     VicPenalty = np.random.uniform(low = .05, high = .3)
     CoerceMob = np.random.uniform(low = .1, high = .5)
     DisloyalPenalty = np.random.uniform(low = .25, high = .75)
-    delta = np.random.uniform(low = .1, high = .5)
+    delta = np.random.uniform(low = .1, high = .75)
     battledeaths = np.random.poisson(1) + 1
     growthrate = .1
     victimerror = np.random.uniform(low = 0, high = .5)
     turnlimit = np.random.poisson(10) + 1
+    govIdeo = 0
     abm = Country("abm", territory, conn, actors, civ)
     try:
       abm.Game()
       abmwriter.writerow([actors, territory, conn, civ, VicPenalty, CoerceMob, delta, battledeaths, growthrate, victimerror, turnlimit, abm.victimhist, abm.attackhist, abm.actorlists])
     except ZeroDivisionError:
       print("next one")
-    if i%10 == 0:
+    if i%100 == 0:
       print(i)
