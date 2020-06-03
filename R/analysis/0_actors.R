51d0a4b20fda73bce40b26dd4f11a21d75bfc9d1
s7minhas <s7.minhas@gmail.com>
2020-06-03 17:29:52 -0400

    desc graphics

--- R/analysis/0_actors.R
+++ R/analysis/0_actors.R
@@ -13,11 +13,16 @@ if(Sys.info()['user'] %in% c('dorffc')){
   source('~/ProjectsGit/victimization/R/setup.R') }
 
 loadPkg(c('readr', 'ggmap', 'abind', 'countrycode'))
+
+smTheme = theme(
+  axis.ticks=element_blank(),
+  panel.border=element_blank()
+)
 ###########################################################
 
 ###########################################################
-acled = read_csv(
-  paste0(pathData, "acled-armedactors-battles.csv"))
+acled = suppressMessages(read_csv(
+  paste0(pathData, "acled-armedactors-battles.csv")))
 ###########################################################
 
 ###########################################################
@@ -145,16 +150,6 @@ actorCntsID = lapply( ids, function(id){
 actorCntsID$year = num(actorCntsID$year)
 actorCntsID$nActors = num(actorCntsID$nActors)
 
-#
-summary(actorCntsID$nActors)
-
-head(actorCntsID)
-actorCntsID %>%
-  group_by(year) %>%
-  summarize(
-    mean(nActors)
-  )
-
 # todos
 ## make some charts to explore the "average" that we calcd
 ## make a map
@@ -169,11 +164,6 @@ actorCntsID %>%
 
 # now make a map that is dumb w/o further revisions
 world <- map_data("world")
-worldplot <- ggplot() +
-  geom_polygon(data = world, aes(x=long, y = lat, group = group)) +
-  coord_fixed(1.3) +
-  theme_void()
-worldplot
 
 ## check for disagreements between the two datasets
 # use countrycode pkg to stdz names across
@@ -181,43 +171,134 @@ worldplot
 world$cname = countrycode(
   world$region, 'country.name', 'country.name'
 )
-# unique(world$region[is.na(world$cname)])
 world = world[!is.na(world$region),]
+cntriesToDrop = c(
+  'ANTARCTICA', 'GREENLAND'
+)
+world = world[!world$cname %in% cntriesToDrop,]
 
 # do the same for acled
 actorCntsID$cname = countrycode(
   actorCntsID$country, 'country.name', 'country.name'
 )
 
-diff <- setdiff(world$region, groups$country) #ugh lots to clean
-
-#if I do not fix above^ this is wrong:
-names(world)[names(world) == "region"] <- "country"
-worldSubset <- inner_join(world, groups, by = "country")
-head(worldSubset)
-
-worldSubset %>%
-  filter(number_groups>10)
+#
+yrs = sort(unique(actorCntsID$year))
+worldBig = lapply(yrs, function(t){
+  slice = actorCntsID[actorCntsID$year==t,]
+  world$year = t
+  world$nActors = slice$nActors[match(world$cname, slice$cname)]
+  return(world)
+}) %>% do.call('rbind', .)
 
 ## theme
-basic <- theme(
-  axis.text = element_blank(),
-  axis.line = element_blank(),
-  axis.ticks = element_blank(),
-  panel.border = element_blank(),
-  panel.grid = element_blank(),
-  axis.title = element_blank(),
-  panel.background = element_rect(fill = "white"),
-  plot.title = element_text(hjust = 0.5)
+worldSlice = worldBig[worldBig$year %in% seq(1999,2019,4),]
+worldGroups = ggplot() +
+  geom_polygon(
+    data = worldSlice,
+    mapping = aes(
+      x = long,
+      y = lat,
+      group = group,
+      fill = nActors
+  ) ) +
+  coord_fixed(1.3) +
+  scale_fill_gradient2(
+    low='#f7fbff',
+    mid='#6baed6',
+    high='#08306b',
+    na.value='grey70'
+    ) +
+  labs(
+    fill="# Active \n Armed Groups"
+  ) +
+  facet_wrap(~year) +
+  theme_void() +
+  theme(
+    legend.position='top',
+    legend.key.width=unit(1.5, 'cm')
+  )
+ggsave(
+  worldGroups,
+  file=paste0(pathGraphics,'actorCntMap.pdf'),
+  height=8, width=12
 )
 
-worldGroups <- ggplot(
-  data = worldSubset,
-  mapping = aes(x = long, y = lat, group = group)
+# create animated version
+
+###########################################################
+
+###########################################################
+actorCntsID = actorCntsID %>%
+  group_by(year) %>%
+  mutate(
+    nActorYrAvg = mean(nActors, na.rm=TRUE)
+  )
+
+ggplot(
+  actorCntsID,
+  aes(x=nActors) ) +
+  geom_histogram() +
+  geom_vline(
+    aes(xintercept=nActorYrAvg),
+    color='red',
+    linetype='dashed'
   ) +
-  coord_fixed(1.3) +
-  geom_polygon(aes(fill = number_groups)) +
-  scale_fill_distiller(palette ="BrBG", direction = -1) +
-  ggtitle("Number of warring parties") +
-  basic
+  facet_wrap(~year) +
+  theme(
+    panel.border=element_blank(),
+    axis.ticks=element_blank()
+  )
+###########################################################
+
+###########################################################
+# boxplot summary of actor dist over time
+ggplot(
+  actorCntsID,
+  aes(x=factor(year), y=nActors)
+  ) +
+  geom_boxplot(outlier.shape=NA) +
+  geom_jitter(alpha=.3) +
+  geom_hline(
+    aes(yintercept=5),
+    color='red',
+    linetype='dashed'
+  ) +
+  labs(
+    x='', y='# Active Armed Groups'
+  ) +
+  theme(
+    axis.text.x=element_text(angle=45),
+    panel.border=element_blank(),
+    axis.ticks=element_blank()
+  )
+###########################################################
+
+###########################################################
+# create categorical summary of actors over time
+actorCntsID$grFive = 1*(actorCntsID$nActors>5)
+actorCntsID$nCat = '1-4'
+actorCntsID$nCat[actorCntsID$nActors>5] = '5-9'
+actorCntsID$nCat[actorCntsID$nActors>10] = '>10'
+actorCntsID$nCat = factor(
+  actorCntsID$nCat,
+  levels=rev(c('1-4','5-9','>10'))
+)
+
+ggplot(actorCntsID,
+  aes(
+    x=factor(year),
+    fill=factor(nCat)
+  )) +
+  geom_bar() +
+  scale_fill_brewer(palette='Dark2') +
+  labs(
+    x='',
+    y='# Active Armed Groups',
+    fill=''
+  ) +
+  smTheme +
+  theme(
+    axis.text.x=element_text(angle=45)
+  )
 ###########################################################
