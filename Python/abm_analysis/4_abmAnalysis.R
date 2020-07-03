061778fb89509d4b2aeb5305603b598ceaeb891b
s7minhas <s7.minhas@gmail.com>
2020-07-03 19:06:39 -0400

    cleanup abm analysis

--- Python/abm_analysis/4_abmAnalysis.R
+++ Python/abm_analysis/4_abmAnalysis.R
@@ -8,8 +8,8 @@ if(Sys.info()['user'] %in% c('s7m', 'janus829')){
 if(Sys.info()['user'] %in% c('maxgallop')){
 	source('~/Documents/victimization/R/setup.R') }
 
-loadPkg(c('MASS'
-	# ,'KRLS','bigKRLS','randomForest'
+loadPkg(c(
+	'MASS', 'glmmTMB'
 	))
 ################################################
 
@@ -17,36 +17,23 @@ loadPkg(c('MASS'
 load(paste0(abmPath, 'netStats.rda'))
 ################################################
 
-cor(netStats[,c("numConf", 'graph_avgDeg', 'graph_dens', 'n_actors')])
-
 ################################################
-# run neg binom
-# mod = glm.nb(
-# 	vic ~ graph_avgDeg + numConf + n_actors +  graph_centrz,
-# 	data=netStats)
-#
-# mod_pois = glm(
-# 	vic ~ graph_avgDeg + numConf + n_actors +  graph_centrz,
-# 	data=netStats,
-# 	family='poisson')
-# summary(mod)$'coefficients'
-# summary(mod_pois)$'coefficients'
-
-head(netStats)
-
-cbind(names(netStats))
-
-loadPkg('glmmTMB')
-
-# run across relev net stat vars
+# check mean and var of vic count
+# to justify use of neg binom
 mean(netStats$vic)
 var(netStats$vic)
+################################################
 
+################################################
+# convert game counter to factor
+# for RE model
 netStats$game = factor(netStats$game)
 
+# choose vars to test
 vars = names(netStats)[1:11]
 perfVars = vars[c(1,3,6)]
 
+# run in parallel
 loadPkg(c('foreach','doParallel'))
 cores = length(perfVars)
 cl = makeCluster(cores)
@@ -65,7 +52,9 @@ return(out)
 stopCluster(cl)
 names(res) = perfVars
 res
+################################################
 
+################################################
 # compare performance of metrics via cross val
 loadPkg(c('doParallel', 'foreach'))
 nFolds = 10
@@ -74,43 +63,13 @@ set.seed(6886)
 netStats$fold = sample(folds, nrow(netStats), TRUE)
 parDF = expand.grid(perfVars, folds, stringsAsFactors=FALSE)
 
-#
-netStatsAgg = netStats %>%
-	group_by(game) %>%
-	summarize(
-		vic = sum(vic, na.rm=TRUE),
-		graph_dens = mean(graph_dens,na.rm=TRUE),
-		graph_avgDeg = mean(graph_avgDeg, na.rm=TRUE),
-		graph_localTrans = mean(graph_localTrans, na.rm=TRUE),
-		numConf = sum(numConf, na.rm=TRUE),
-		n_actors = max(n_actors, na.rm=TRUE)
-	) %>% data.frame(.,stringsAsFactors=FALSE)
-
-head(netStatsAgg)
-
-mean(netStatsAgg$vic)
-var(netStatsAgg$vic)
-netStatsAggStdz = apply(netStatsAgg[,-which('vic'==names(netStatsAgg))], 2,
-	function(x){(x-mean(x, na.rm=TRUE))/sd(x,na.rm=TRUE)})
-netStatsAggStdz = data.frame(netStatsAggStdz, stringsAsFactors=FALSE)
-netStatsAggStdz$vic = netStatsAgg[,'vic']
-
-vars = names(netStats)[1:11]
-perfVars = vars[c(1,3,6)]
-res = lapply(perfVars, function(v){
-  form=formula(paste0('vic~numConf+n_actors+', v))
-  mod = glm.nb(form, data=netStatsAggStdz)
-  out = summary(mod)$'coefficients'
-  return(out)
-  })
-names(res) = perfVars
-res
-
-
 # run in parallel
 cl = makeCluster(20)
 registerDoParallel(cl)
-perfRes = foreach(ii = 1:nrow(parDF), .packages=c('MASS')) %dopar% {
+perfRes = foreach(
+	ii = 1:nrow(parDF),
+	.packages=c('MASS', 'glmmTMB')
+	) %dopar% {
 	# get instr from parDF
 	v = parDF[ii,1] ; f = parDF[ii,2]
 
@@ -137,79 +96,9 @@ stopCluster(cl)
 
 # org results
 perfRes = do.call('rbind', perfRes)
+########################################################
 
-# quick analysis
-res
-perfRes %>% group_by(var) %>% summarize(mean(rmse))
-ggplot(perfRes, aes(x=var, y=rmse)) +
-	geom_point()
-
-summary(netStats$vic)
-sd(netStats$vic)
-mean(netStats$vic)
-
-head(netStats)
-dim(netStats)
-cbind(names(netStats))
-
-toDrop = -which(names(netStats) %in% c('game', 'turn', 'fold', 'id', 'V1', 'V10', 'V9','graph_meanDist', 'graph_dens'))
-
-slice = na.omit(netStats[,toDrop])
-y = slice[,'vic']
-ftrs = slice[,-which(names(slice) %in% 'vic')]
-ftrs = apply(ftrs, 2, function(x){ (x-mean(x))/sd(x) })
-
-library(glmnet)
-lassoMod = cv.glmnet(x=data.matrix(
-	ftrs,
-	), y=y, family='poisson', alpha=1)
-coef(lassoMod)
-# nonlinMod=krls(
-# 	X=data.matrix(
-# 		netStats[,c('graph_dens', 'numConf', 'n_actors')]
-# 		),
-# 	y=netStats[,'vic']
-# 	)
-
-# nonlinMod2=bigKRLS(
-# 	X=data.matrix(
-# 		netStats[,c('graph_dens', 'numConf', 'n_actors')]
-# 		),
-# 	y=netStats[,'vic']
-# 	)
-
-library(randomForest)
-rfMod = randomForest(x=data.matrix(
-	netStats[,c('graph_dens', 'numConf', 'n_actors', 'graph_avgDeg')]
-	), y=netStats[,'vic'], type='regression')
-
-preds = predict(rfMod, type='response')
-head(preds)
-sqrt( mean( (preds - netStats[,'vic'])^2 ) )
-# partialPlot(rfMod, pred.data=netStats, x.var='graph_dens')
-
-# library(BART)
-# set.seed(6886)
-# nd = 200
-# burn = 50
-# x = data.matrix(
-# 	netStats[,c('graph_dens', 'numConf', 'n_actors')]
-# 	)
-# y = netStats[,'vic']
-# post = wbart(x, y, nskip=burn, ndpost=nd)
-
-# plot(post$sigma, type="l")
-# abline(v=burn, lwd=2, col="red")
-
-# library(BayesTree)
-
-# pdb1 = pdbart(
-# 	x,y,xind=c(1,2),
-# 	levs=list(seq(-1,1,.2),seq(-1,1,.2)),pl=FALSE,
-#should run longer!
-# 	keepevery=10,ntree=100,nskip=100,ndpost=200)
-# plot(pdb1,ylim=c(-.6,.6))
-
+########################################################
 # viz of results
 raw = summary(mod)$'coefficients'[-1,]
 coefData = raw %>%
@@ -232,9 +121,7 @@ coefData$varName = factor(
 	coefData$varName,
 	levels=varKey$clean
 	)
-########################################################
 
-########################################################
 # viz
 ggCoef = ggplot(
 		coefData,
