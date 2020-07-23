9dc32a5a42398463ebb0cc84d76cdccb8b04433d
s7minhas <s7.minhas@gmail.com>
2020-07-22 23:05:39 -0400

    update floats

--- R/analysis/3_modelViz.R
+++ R/analysis/3_modelViz.R
@@ -6,9 +6,9 @@ if(Sys.info()['user'] %in% c('Owner','herme','S7M')){
 ########################################################
 
 ########################################################
-load(paste0(pathResults, 'baseMods_v3.rda'))
-load(paste0(pathResults, 'cnt1Mods_v3.rda'))
-load(paste0(pathResults, 'cnt2Mods_v3.rda'))
+load(paste0(pathResults, 'baseMods.rda'))
+load(paste0(pathResults, 'cnt1Mods.rda'))
+load(paste0(pathResults, 'cnt2Mods.rda'))
 ########################################################
 
 ########################################################
@@ -31,8 +31,8 @@ coefImpRE = list(
 
 ########################################################
 mLabs = c(
-	'Base ACLED Model (1997-2020)',
-	'Base + Controls (1997-2018)', 'Base + Controls (1997-2012)' )
+	'Base ACLED Model',
+	'Base + Controls\n(1997-2018)', 'Base + Controls\n(1997-2012)' )
 
 varKey = data.frame(
 	dirty=rownames(coefMissFE$'cnt2'), stringsAsFactors = FALSE )
@@ -91,11 +91,26 @@ coefViz = function(coefData, fName, path=pathGraphics){
 				fill = "#525252", color='#525252'))
 	ggsave(ggCoef, width=8, height=6,
 		file=paste0(path, fName), device=cairo_pdf) }
+########################################################
 
-coefViz(ggDataImpFE, fName='coefPlotImpFE_v3.pdf')
-coefViz(ggDataImpRE, fName='coefPlotImpRE_v3.pdf')
-coefViz(ggDataMissFE[ggDataMissFE$model!=mLabs[1],],
-	fName='coefPlotMissFE_v3.pdf')
-coefViz(ggDataMissRE[ggDataMissRE$model!=mLabs[1],],
-	fName='coefPlotMissRE_v3.pdf')
+########################################################
+# comparison of base model: fe vs re
+baseFE = ggDataMissFE[ggDataMissFE$model=='Base ACLED Model',]
+baseRE = ggDataMissRE[ggDataMissRE$model=='Base ACLED Model',]
+baseFE$model = 'Base ACLED Model\n(Fixed Country Effects)'
+baseRE$model = 'Base ACLED Model\n(Random Country Effects)'
+ggDataBaseMod = rbind(baseFE, baseRE)
+coefViz(ggDataBaseMod, 'coefPlotBase_FE_RE.pdf')
+
+# re mods with cntrls
+cntImpRE = ggDataImpRE[ggDataImpRE$model!=mLabs[1],]
+cntMissRE = ggDataMissRE[ggDataMissRE$model!=mLabs[1],]
+coefViz(cntImpRE, 'coefPlotCntImpRE.pdf')
+coefViz(cntMissRE, 'coefPlotCntMissRE.pdf')
+
+# fe mods with cntrls
+cntImpFE = ggDataImpFE[ggDataImpFE$model!=mLabs[1],]
+cntMissFE = ggDataMissFE[ggDataMissFE$model!=mLabs[1],]
+coefViz(cntImpFE, 'coefPlotCntImpFE.pdf')
+coefViz(cntMissFE, 'coefPlotCntMissFE.pdf')
 ########################################################
