81fa541fd8eb797201f5caafd873116df2c96152
s7minhas <s7.minhas@gmail.com>
2020-07-29 09:02:50 -0400

    push up manu changes

--- R/desc/netVizVic.R
+++ R/desc/netVizVic.R
@@ -13,6 +13,8 @@ library(igraph)
 library(network)
 
 set.seed(12345)
+par(mfrow=c(1, 2), mar=c(0,0,0,0))
+pdf(file='C:/Users/herme/net3_2_simple.pdf')
 #low vic
 gFirst <- graph.formula(1-2, 3-4, 5-6,  7-8, 8, 9, 8-10)
 V(gFirst)$color <- "gray26"
@@ -21,14 +23,14 @@ gFirst$layout <- coords
 E(gFirst)$weight <-c(1, 1, .5, 1,1)
 plot(gFirst, vertex.label=NA, main="Low Victimization", edge.width=E(gFirst)$weight*3, layout=s, edge.curved=0.1)
 
-#med vic
-gSec <- graph.formula(2-4, 3-4, 4-5, 4-6, 5-4, 6-5, 7-5, 8-4, 9-4, 10-4)
-V(gSec)$color <- "gray26"
-E(gSec)$weight <-c(.5, .5, .5, 1, 1, 1, .5,.5,.5)
-#l <- layout_in_circle(gMed)
-#s <- layout_with_kk(gMed)
-plot(gSec, vertex.label=NA, main="Moderate Victimization",
-		layout=s, edge.width=E(gSec)$weight*2, edge.curved=0.1)
+# #med vic
+# gSec <- graph.formula(2-4, 3-4, 4-5, 4-6, 5-4, 6-5, 7-5, 8-4, 9-4, 10-4)
+# V(gSec)$color <- "gray26"
+# E(gSec)$weight <-c(.5, .5, .5, 1, 1, 1, .5,.5,.5)
+# #l <- layout_in_circle(gMed)
+# #s <- layout_with_kk(gMed)
+# plot(gSec, vertex.label=NA, main="Moderate Victimization",
+# 		layout=s, edge.width=E(gSec)$weight*2, edge.curved=0.1)
 
 #high vic
 gLast <- graph.formula(1-2, 3-4, 1-3, 1-4, 1-5, 2-3, 2-4, 2-5, 3-4, 3-5, 4-1, 2-4, 4-5, 4-6, 10-4, 8-4, 10-4,
@@ -37,5 +39,4 @@ V(gLast)$color <- "gray26"
 E(gLast)$weight <- runif(ecount(gLast)) #random value from uniform for each edge
 plot(gLast, vertex.label=NA, main="High Victimization",
 		layout=s, edge.width=E(gLast)$weight, edge.curved=0.1)
-
-par(mfrow=c(1,3))
+dev.off()
