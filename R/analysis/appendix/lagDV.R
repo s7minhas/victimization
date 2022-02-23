06942bf1d4dcadace3282e452a5940e21b54b080
s7minhas <s7.minhas@gmail.com>
2022-02-23 10:14:01 -0500

    lag dv plots

--- R/analysis/appendix/lagDV.R
+++ R/analysis/appendix/lagDV.R
@@ -22,23 +22,97 @@ dataCnt2 = cbind(dataCnt2, lagVar(dataCnt2, dv))
 ########################################################
 
 ########################################################
-# set ivs
+# mod spec
 ivs = c(paste0('lag1_',dv), 'herf', 'nConf', 'nActors')
-lagBase = glm.nb(f(dv, ivs), data=dataBase)
-summary(lagBase)$'coefficients'
+p = length(ivs)
+feF = formula(paste0(
+  dv, '~', paste(ivs, collapse='+'), '+factor(cname)-1'))
+reF = formula(paste0(
+  dv, '~', paste(ivs, collapse='+'), '+(1|cname)'))
+
+# get mods
+feModBase = glm.nb(feF, data=dataBase)
+reModBase = glmmTMB(reF, data=dataBase, family='nbinom2')
+
+# get coefs
+feModBaseCoef = summary(feModBase)$'coefficients'[1:p,]
+reModBaseCoef = summary(reModBase)$'coefficients'$cond[1:(p+1),]
 ########################################################
 
 ########################################################
+# mod spec
 ivs = c( ivs,
   'polity2', 'popLog', 'gdpCapLog',
   'exclpop', 'anyPeaceKeeper' )
-lagCnt1 = glm.nb(f(dv,ivs), data=dataCnt1)
-summary(lagCnt1)$'coefficients'
+p = length(ivs)
+feF = formula(paste0(
+  dv, '~', paste(ivs, collapse='+'), '+factor(cname)-1'))
+reF = formula(paste0(
+  dv, '~', paste(ivs, collapse='+'), '+(1|cname)'))
+
+# get mods
+feModCnt1 = glm.nb(feF, data=dataCnt1)
+reModCnt1 = glmmTMB(reF, data=dataCnt1, family='nbinom2')
+
+# get coefs
+feModCnt1Coef = summary(feModCnt1)$'coefficients'[1:p,]
+reModCnt1Coef = summary(reModCnt1)$'coefficients'$cond[1:(p+1),]
 ########################################################
 
 ########################################################
+# mod spec
 ivs = c( ivs,
   'rebsStronger', 'rebSupportGov', 'govSupportGov')
-lagCnt2 = glm.nb(f(dv,ivs), data=dataCnt2)
-summary(lagCnt2)$'coefficients'
+p = length(ivs)
+feF = formula(paste0(
+  dv, '~', paste(ivs, collapse='+'), '+factor(cname)-1'))
+reF = formula(paste0(
+  dv, '~', paste(ivs, collapse='+'), '+(1|cname)'))
+
+# get mods
+feModCnt2 = glm.nb(feF, data=dataCnt2)
+reModCnt2 = glmmTMB(reF, data=dataCnt2, family='nbinom2')
+
+# get coefs
+feModCnt2Coef = summary(feModCnt2)$'coefficients'[1:p,]
+reModCnt2Coef = summary(reModCnt2)$'coefficients'$cond[1:(p+1),]
 ########################################################
+
+############################
+# get viz
+
+# org coefs
+coefFE = list(
+	base=feModBaseCoef,
+	cnt1=feModCnt1Coef,
+	cnt2=feModCnt2Coef )
+
+coefRE = list(
+	base=reModBaseCoef,
+	cnt1=reModCnt1Coef,
+	cnt2=reModCnt2Coef )
+
+# add labels
+mLabs = c(
+	'Base ACLED Model',
+	'Base + Controls\n(1997-2018)', 'Base + Controls\n(1997-2015)' )
+
+varKey = data.frame(
+	dirty=rownames(coefFE$'cnt2'), stringsAsFactors = FALSE )
+varKey$clean = c(
+	'Lag DV',
+	'Network\nCompetition', 'Number of\nConflicts', 'Number of\nActors',
+	'Polity', 'Log(Population)', 'Log(GDP per Capita)',
+	'Excluded\nPopulation', 'Presence of\nPeacekeepers',
+	'Rebel(s) Stronger\nthan Govt.',
+	'Rebel(s) Supported\nby Foreign Govt.',
+	'Govt. Supported\nby Foreign Govt.' )
+
+# process coefs
+ggDataFE = coefProcess(coefFE)
+ggDataRE = coefProcess(coefRE)
+
+# create and save viz to pathGraphics
+coefViz(ggDataFE, 'coefPlot_FE_lagDV.pdf')
+coefViz(ggDataRE, 'coefPlot_RE_lagDV.pdf')
+############################
