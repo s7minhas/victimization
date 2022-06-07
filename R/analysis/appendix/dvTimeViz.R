03344955c9c0cf412db928f51722497d52411895
s7m <s7.minhas@gmail.com>
2022-06-07 14:41:17 -0400

    incorp final reviewer comments in manu

--- R/analysis/appendix/dvTimeViz.R
+++ R/analysis/appendix/dvTimeViz.R
@@ -34,27 +34,81 @@ ggMed = ggplot(dataBase, aes(x=factor(year), y=herf) ) +
 		)	+
 	geom_jitter(
 		data=dataBase,
-		alpha=.15, width=.1) +
-	# geom_line(
-	# 	data=yrAgg, aes(x=factor(year), y=herfMed, group=1),
-	# 	size=.4 ) +
+		alpha=.1, width=.25) +
+	geom_line(
+		data=yrAgg, aes(x=factor(year), y=herfMed, group=1),
+		size=.8, color='grey50' ) +
 	geom_point(data=yrAgg,
 		aes(x=factor(year), y=herfMed),
 		size=20, shape='-', color='grey40' ) +
 	xlab('') + ylab('Network Competition') +
-	scale_x_discrete(breaks=yrBrks) +
+	# scale_x_discrete(breaks=yrBrks) +
 	theme_light(base_family="Source Sans Pro") +
 	theme(
 		legend.position='top', legend.title=element_blank(),
 		panel.border=element_blank(), axis.ticks=element_blank(),
 		axis.text.y=element_text(hjust=0)
 	)
-ggsave(
-	ggMed,
-	width=8, height=5,
-	file=paste0(pathGraphics, 'netCompMed.pdf'),
-	device=cairo_pdf )
+# ggsave(
+# 	ggMed,
+# 	width=8, height=5,
+# 	file=paste0(pathGraphics, 'netCompMed.pdf'),
+# 	device=cairo_pdf )
 ####################################
+
+###########################################################
+load(paste0(pathData, 'actorCntsID.rda'))
+###########################################################
+
+###########################################################
+# categorical vars
+actorCntsID$nCat3 = addCat(
+	actorCntsID$nActors,
+	c('4 or\nless','5-9','10 or\nmore actors'), c(5,10))
+###########################################################
+
+###########################################################
+# viz diff in # actors over time via cat variable
+actorCats = ggplot(actorCntsID,
+  aes(
+    x=factor(year),
+    fill=factor(nCat3)
+  )) +
+  geom_bar(colour='grey40') +
+  scale_fill_brewer(palette='Greys') +
+  labs(
+    x='',
+    y='# Countries',
+    fill=''
+  ) +
+	theme_light(base_family="Source Sans Pro") +
+  smTheme +
+  theme(
+    axis.text.x=element_text(angle=45),
+	 	legend.position='top' )
+# ggsave(actorCats,
+#   file=paste0(pathGraphics, 'actorCntCats.pdf'),
+#   width=8, height=6, device=cairo_pdf)
+###########################################################
+
+###########################################################
+#
+loadPkg('patchwork')
+top = actorCats + theme(
+	axis.text.x=element_blank(),
+	panel.grid.minor=element_blank()
+	,plot.margin = unit(c(0,0,-10,0), "cm")
+)
+bottom = ggMed + theme(
+	axis.text.x=element_text(angle=90),
+	panel.grid.minor=element_blank()
+	,plot.margin = unit(c(-10,0,0,0), "cm")
+)
+ggViz = top/bottom
+ggsave(ggViz,
+	file=paste0(pathGraphics, 'netCompDesc.pdf'),
+	width=8, height=6, device=cairo_pdf )
+###########################################################
 
 ########################################################
 # viz over time by countries
@@ -68,7 +122,7 @@ ggCntry = ggplot(
   geom_line(size=.4) + geom_point(size=.6) +
   facet_wrap(~cabb, scale='free_y') +
 	xlab('') + ylab('Network Competition by Country') +
-	scale_x_continuous(breaks=yrBrks) + 
+	scale_x_continuous(breaks=yrBrks) +
 	theme_light(base_family="Source Sans Pro") +
 	theme(
 		legend.position='top', legend.title=element_blank(),
